84aef8126d3221bd020b7f39f90a3df4b9cf527c
danishmalik419 <danissh419@gmail.com>
2025-01-15 18:10:09 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cors(corsOptions)); // Apply CORS middleware
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Welcome to the Home Page!');
 });
 
