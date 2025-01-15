a6c11b3b5a83bdb2f001d15d4d62595eae87eeb1
danishmalik419 <danissh419@gmail.com>
2025-01-15 18:44:54 +0530

    first commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Set base URL
+  baseURL: 'https://app-4p89.onrender.com/api', // Set base URL
   headers: {
     'Content-Type': 'application/json',
   },
